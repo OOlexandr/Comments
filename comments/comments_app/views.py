5d3178601fdfdb1c68bbb637b1d7d2ee1d3838d1
Alex <oleksarkhumua@gmail.com>
2024-06-25 20:27:51 +0300

    pages.
    added pages. For now every page has two comments (for easier testing). Also broke sorting.

--- comments/comments_app/views.py
+++ comments/comments_app/views.py
@@ -1,27 +1,24 @@
 from django.shortcuts import render, redirect
 from comments_app.forms import CommentForm
 from comments_app.models import Comment
+from math import ceil
+
+PAGE_SIZE = 2
 
 def view_comment(request):
-    print(request.GET)
-    order_by = request.GET.get("order_by")
-    page = request.GET.get("page") if request.GET.get("page") else 0
-    if order_by:
-        comments = Comment.objects.filter(parent__isnull=True).order_by(request.GET.get("order") + order_by)[page*25:(page+1)*25]
-    else:
-        comments = Comment.objects.filter(parent__isnull=True).order_by('-created')[page*25:(page+1)*25]
+    order_by = request.GET.get("order_by") if request.GET.get("order_by") else "created"
+    order = request.GET.get("order") if request.GET.get("order") else "-"
+    page = int(request.GET.get("page")) if request.GET.get("page") else 0
+    n_pages = ceil(Comment.objects.filter(parent__isnull=True).__len__() / PAGE_SIZE) - 1
+    comments = Comment.objects.filter(parent__isnull=True).order_by(order + order_by)[page*PAGE_SIZE:(page+1)*PAGE_SIZE]
     form = CommentForm()
-    return render(request, 'comments_app/view_comments.html', {'comments': comments, 'form': form})
+    return render(request, 'comments_app/view_comments.html', {'comments': comments, 'form': form, "order_by": order_by,
+                                                               "order": order, "page": page, "n_pages": n_pages})
 
 def add_comment(request):
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect(to='comments_app:view_comments')
-        else:
-            comments = Comment.objects.filter(parent__isnull=True)
-            return render(request, 'comments_app/view_comment.html', {'comments': comments, 'form': form})
 
-    comments = Comment.objects.filter(parent__isnull=True)
-    return render(request, 'comments_app/view_comment.html', {'comments': comments, 'form': CommentForm()})
+    return redirect(to='comments_app:view_comments')

7d4ed410b7c4eb4207e5ca9cb164521c8e8cd59b
Alex <oleksarkhumua@gmail.com>
2024-06-25 20:30:59 +0300

    fixed sorting

--- comments/comments_app/views.py
+++ comments/comments_app/views.py
@@ -7,11 +7,12 @@ PAGE_SIZE = 2
 
 def view_comment(request):
     order_by = request.GET.get("order_by") if request.GET.get("order_by") else "created"
-    order = request.GET.get("order") if request.GET.get("order") else "-"
+    order = request.GET.get("order") if request.GET.get("order") != None else "-"
     page = int(request.GET.get("page")) if request.GET.get("page") else 0
     n_pages = ceil(Comment.objects.filter(parent__isnull=True).__len__() / PAGE_SIZE) - 1
     comments = Comment.objects.filter(parent__isnull=True).order_by(order + order_by)[page*PAGE_SIZE:(page+1)*PAGE_SIZE]
     form = CommentForm()
+    print(order+order_by)
     return render(request, 'comments_app/view_comments.html', {'comments': comments, 'form': form, "order_by": order_by,
                                                                "order": order, "page": page, "n_pages": n_pages})
 

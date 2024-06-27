92c798664afcb39fe4e480308f526f211ea6be10
Alex <oleksarkhumua@gmail.com>
2024-06-27 12:55:31 +0300

    changed replies
    Now adding replies does not reload the page so the user doesn't loose the comment they replied to.

--- comments/comments_app/views.py
+++ comments/comments_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 from comments_app.forms import CommentForm
 from comments_app.models import Comment
 from math import ceil
@@ -12,14 +14,17 @@ def view_comment(request):
     n_pages = ceil(Comment.objects.filter(parent__isnull=True).__len__() / PAGE_SIZE) - 1
     comments = Comment.objects.filter(parent__isnull=True).order_by(order + order_by)[page*PAGE_SIZE:(page+1)*PAGE_SIZE]
     form = CommentForm()
-    print(order+order_by)
     return render(request, 'comments_app/view_comments.html', {'comments': comments, 'form': form, "order_by": order_by,
                                                                "order": order, "page": page, "n_pages": n_pages})
 
 def add_comment(request):
     if request.method == 'POST':
         form = CommentForm(request.POST)
-        if form.is_valid() and request.POST.get("captcha") == request.POST.get("captcha_answer"):
-            form.save()
+        if form.is_valid() and request.POST.get("captcha") == request.POST.get("captcha_answer") and request.POST.get("captcha"):
+            comment = form.save()
+            if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+                comment_html = render_to_string('comments_app/comment.html', {'comment': comment}, request=request)
+                return JsonResponse({'comment_html': comment_html})
 
+    print("not here")
     return redirect(to='comments_app:view_comments')
